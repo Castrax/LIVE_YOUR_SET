099aeb64800a5a592d3c70431cfc3db5f64bd03d
Mathilde Robert <m.robert@skema.edu>
2019-11-20 12:26:36 +0100

    map welldone

--- app/controllers/movie_sets_controller.rb
+++ app/controllers/movie_sets_controller.rb
@@ -2,7 +2,14 @@ class MovieSetsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
-    @movie_sets = MovieSet.all
+    @movie_sets = MovieSet.geocoded
+
+    @markers = @movie_sets.map do |movie|
+      {
+        lat: movie.latitude,
+        lng: movie.longitude
+      }
+    end
   end
 
   def show
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,14 @@
 import "bootstrap";
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+import 'mapbox-gl/dist/mapbox-gl.css';
 
 flatpickr(".datepicker", {
   mode: "range",
   dateFormat: "d/m/Y",
   minDate: "today",
 });
+
+import { initMapbox } from '../plugins/init_mapbox';
+
+initMapbox();
