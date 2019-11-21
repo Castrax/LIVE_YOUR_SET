0f4d421ce4e60645be7ac0df4ada8ca7ebedd95a
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-11-21 15:15:30 +0100

    modal made but missing creation booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,18 +1,31 @@
 class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
-    @user = User.find(params[:user_id])
-    @booking.user = @user
+    # @user = current_user
+    # @booking.user = @user
+    #     const end_date = new Date(string_end_date.value);
+    # const start_milliseconds = start_date.getTime();
+    # const end_milliseconds = end_date.getTime();
+    # const milliseconds = end_milliseconds - start_milliseconds;
+    # const period = milliseconds/86400000;
+    # const totalcalculation = period * price_per_day.innerText
+    @booking.movie_set = MovieSet.find(params[:movie_set_id].to_i)
+    @price_per_day = @booking.movie_set.price_per_day
+    @end_date = params[:booking][:end_date].to_date
+    @start_date = params[:booking][:start_date].to_date
+    @total_price = ((@end_date - @start_date) * @price_per_day).to_i
+    @booking.total_price = @total_price
+    raise
     if @booking.save
-      redirect_to dashboard_path(@user)
+      redirect_to dashboard_path
     else
-      render 'new'
+      redirect_to movie_set_path(params[:movie_set_id].to_i)
+    end
   end
-end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :total_price, :user_id, :movie_set_id)
+    params.require(:booking).permit(:start_date, :end_date, :user_id)
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,32 +4,32 @@ import "flatpickr/dist/flatpickr.min.css";
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 const datepickers = flatpickr(".datepicker", {
-  mode: "range",
   // dateFormat: "d/m/Y",
   minDate: "today",
 });
 
 if (document.querySelector(".datepicker")) {
-  const reservation = document.querySelector(".datepicker")
-const price_per_day = document.querySelector("#price-per-day")
-const total = document.querySelector("#total-price")
+  const string_start_date = document.querySelector("#start-date")
+  const string_end_date = document.querySelector("#end-date")
+  const price_per_day = document.querySelector("#price-per-day")
+  const total = document.querySelector("#total-price")
+  const reservation_days = document.querySelector("#reservation-days")
 
-reservation.addEventListener("change", range =>{
-  console.log("range :", range);
-  const datestring = range.target.value;
-  const datearray = datestring.split(" to ");
-  const start_date = new Date(datearray[0]);
-  const end_date = new Date(datearray[1]);
-  const start_milliseconds = start_date.getTime();
-  const end_milliseconds = end_date.getTime();
-  const milliseconds = end_milliseconds - start_milliseconds;
-  const period = milliseconds/86400000;
-  const totalcalculation = period * price_per_day.innerText
-  total.innerText = totalcalculation;
+string_start_date.addEventListener("change", range =>{
+  const start_date = new Date(string_start_date.value);
+  string_end_date.addEventListener("change", range =>{
+    const end_date = new Date(string_end_date.value);
+    const start_milliseconds = start_date.getTime();
+    const end_milliseconds = end_date.getTime();
+    const milliseconds = end_milliseconds - start_milliseconds;
+    const period = milliseconds/86400000;
+    const totalcalculation = period * price_per_day.innerText
+    total.innerText = totalcalculation;
+    reservation_days.innerText = start_date.toDateString() + " - " + end_date.toDateString();
 });
-
-
+});
 }
+
 
 import { initMapbox } from '../plugins/init_mapbox';
 
